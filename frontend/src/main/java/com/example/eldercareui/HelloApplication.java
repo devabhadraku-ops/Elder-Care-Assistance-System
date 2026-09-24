5dde2cd13f31e24ba72806f8db686161fd02f374
Devabhadra <devabhadraku@gmail.com>
2026-09-24 23:33:27 +0530

    Feature: Add ReminderEngine startup to HelloApplication - backend complete

--- frontend/src/main/java/com/example/eldercareui/HelloApplication.java
+++ frontend/src/main/java/com/example/eldercareui/HelloApplication.java
@@ -4,16 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import com.eldercare.service.ReminderEngine;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        // START REMINDER ENGINE IN BACKGROUND
+        ReminderEngine reminderEngine = new ReminderEngine();
+        reminderEngine.startAutoCheck();
+        System.out.println("✓ Reminder engine started in background");
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Eldercare Assistance System");
         stage.setScene(scene);
         stage.show();
     }
-}
